325a9b43bbe1f7d787c901494fa3fc7f03537b74
Lasse Lang <lang.lasse@gmx.de>
2025-05-29 14:17:41 +0200

    Added some new things

--- src/public/script.js
+++ src/public/script.js
@@ -1,3 +1,5 @@
+const socket = io("/");
+
 const canvas = document.getElementById("canvas");
 
 const width = window.innerWidth;
@@ -15,7 +17,47 @@ ctx.scale(dpi, dpi);
 ctx.font = "20px sans-serif";
 ctx.lineWidth = 2;
 
+let tileset = []
 
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 100);
-ctx.stroke();
+for (i=1;i<22;i++) {
+  let image = new Image()
+  image.src = `/assets/tileset/Pixelcity_64_${i}.bmp`
+  tileset.push(image)
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+class Vector2 {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
+}
+
+tilesetDimensions = new Vector2(18, 10)
+tilemap = []
+let tileSize = 64
+tilesetOffset = new Vector2(50, 50)
+
+
+setTimeout(() => {
+  for (y=0;y<tilesetDimensions.y;y++) {
+    xList = []
+    for (x=0;x<tilesetDimensions.x;x++) {
+      numberGenerated = getRandomInt(21)
+      xList.push(numberGenerated)
+      ctx.drawImage(tileset[numberGenerated], tileSize*x+tilesetOffset.x, tileSize*y+tilesetOffset.y, tileSize, tileSize)
+    }
+    tilemap.push(xList)
+  }
+}, 200)
+
+addEventListener("mousemove", (event) => { 
+  console.log(event.clientX, event.clientY)
+})
+
+// ctx.moveTo(0, 0);
+// ctx.lineTo(200, 100);
+// ctx.stroke();
--- src/server.js
+++ src/server.js
@@ -1,10 +1,20 @@
+http = require("http")
 express = require("express");
 const app = express();
+var server = http.createServer(app);
+var io = require('socket.io')(server);
 
 
 app.use(express.static('public'));
 
 
-app.listen(80, () => {
+server.listen(80, () => {
     console.log("Server running on port 80")
+})
+
+io.on("connection", (socket) => {
+  console.log("User connected: ", socket.id)
+  socket.on("disconnect", () => {
+    console.log("User disconnected: ", socket.id)
+  })
 })
